c95596299e81979551fd182ea961b02de10244ed
RoVora05 <rohan05.vora@gmail.com>
2022-09-01 13:14:59 -0700

    testing

---
+++ futureval.py
@@ -0,0 +1,9 @@
+def main():
+    print ("This program calculates the future value of a 10 year investment.")
+    principal=eval(input("Enter the initial principal: "))
+    rate=eval(input("Enter the interest rate: "))
+    period=eval(input("Enter the period: "))
+    for i in range (10*period):
+        principal*=(1+(rate))
+    print("The value in",years, "years is", principal)
+main()
