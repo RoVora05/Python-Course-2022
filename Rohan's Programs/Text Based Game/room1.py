c1d76f2303cd4b099597c05334389801345e1b0e
RoVora05 <rohan05.vora@gmail.com>
2022-10-20 13:32:55 -0700

    worked on the text based game

--- Rohan's Programs/Text Based Game/room1.py
+++ Rohan's Programs/Text Based Game/room1.py
@@ -1,5 +1,41 @@
+from tbgcentral import *
 def room1():
-    file=open("c:/Users/h4cke/Python Files Clone/Python-Class-Files/Rohan's Programs/Text Based Game/room_descriptions.txt",'r')
-    lines=file.readlines()
-    print(lines[0])
+    invread=open(file_location+"inventory.txt","r")
+    invlist=invread.read().splitlines()
+    invwrite=open(file_location+"inventory.txt","a")
+    
+    
+    print("You can:",options)
+    action=input("What do you do? ")
+    print("")
+
+    if action=="1":
+        file=open(file_location+"room_descriptions")
+        lines=file.readlines()
+        print(lines[0])
+    
+    elif action=="2":
+        print("your inventory contains: ")
+        print(invlist)
+    
+    elif action=="3":
+        useitem=input("Which item will you use? ")
+        if useitem=="key" or useitem=="Key" and invlist[1]=="Key":
+            invwrite.write("")
+    
+    elif action=="4":
+        interact=input("What in the room would you like to interact with? ")
+        if interact=="key" or "Key":
+            print("You pick up the key.")
+            invwrite.write("Key")
+        else:
+            print("You can't interact with",interact)
+    
+    elif action=="5":
+        move=input("Forward or back? ")
+    
+    else:
+        print("Sorry, that wasn't an option.")
+    
+    print("")
 room1()
